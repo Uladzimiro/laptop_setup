ae42efc0848139c997ac81c4562a5f8de5c7c361
Uladzimiro <dvaraf@gmail.com>
2019-12-04 22:12:50 +0100

    Initial install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install tmux
+brew install vim
+brew install git
+git config --global user.name "xxx"
+git config --global user.email "xxx@gmail.com"
+git config --global color.ui true 
